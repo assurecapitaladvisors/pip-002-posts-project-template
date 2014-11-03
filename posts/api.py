6c735c934c74b414bfed4b6863155a866a4056b3
Geoff <assurecapitaladvisors@gmail.com>
2014-11-03 02:42:28 +0000

    Added POST methods

--- posts/api.py
+++ posts/api.py
@@ -8,6 +8,15 @@ import decorators
 from posts import app
 from database import session
 
+# JSON Schema describing the structure of a post
+post_schema = {
+    "properties": {
+        "title" : {"type" : "string"},
+        "body": {"type": "string"}
+    },
+    "required": ["title", "body"]
+}
+
 @app.route("/api/posts", methods=["GET"])
 @decorators.accept("application/json")
 def posts_get():
@@ -45,3 +54,64 @@ def post_get(id):
     # Return the post as JSON
     data = json.dumps(post.as_dictionary())
     return Response(data, 200, mimetype="application/json")
+
+@app.route("/api/posts/<int:id>", methods=["PUT"])
+@decorators.accept("application/json")
+@decorators.require("application/json")
+def posts_put(id):
+
+    data = request.json                                      
+    # Check that the JSON supplied is valid
+    # If not we return a 422 Unprocessable Entity
+    try:
+        validate(data, post_schema)
+    except ValidationError as error:
+        data = {"message": error.message}
+        return Response(json.dumps(data), 422, mimetype="application/json")                               
+          
+    post = session.query(models.Post).get(id)
+    # Check whether the post exists
+    # If not return a 404 with a helpful message
+    if not post:
+        message = "Could not find post with id {}".format(id)
+        data = json.dumps({"message": message})
+        return Response(data, 404, mimetype="application/json")
+                                          
+    post.title = data["title"]
+    post.body = data["body"]
+    session.add(post)
+    session.commit()
+                                          
+    # Return a 201 Created, containing the post as JSON and with the
+    # Location header set to the location of the post
+    data = json.dumps(post.as_dictionary())
+    headers = {"Location": url_for("post_get", id=post.id)}
+    return Response(data, 201, headers=headers,
+                    mimetype="application/json")
+
+@app.route("/api/posts", methods=["POST"])
+@decorators.accept("application/json")
+@decorators.require("application/json")
+def posts_post():
+    """ Add a new post """
+    data = request.json
+
+    # Check that the JSON supplied is valid
+    # If not we return a 422 Unprocessable Entity
+    try:
+        validate(data, post_schema)
+    except ValidationError as error:
+        data = {"message": error.message}
+        return Response(json.dumps(data), 422, mimetype="application/json")
+
+    # Add the post to the database
+    post = models.Post(title=data["title"], body=data["body"])
+    session.add(post)
+    session.commit()
+
+    # Return a 201 Created, containing the post as JSON and with the
+    # Location header set to the location of the post
+    data = json.dumps(post.as_dictionary())
+    headers = {"Location": url_for("post_get", id=post.id)}
+    return Response(data, 201, headers=headers,
+                    mimetype="application/json")
